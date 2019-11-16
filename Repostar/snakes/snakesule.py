43972d6d9b6a81dfb6f5937cb80ee699cf6d19d0
naponoel <lekileo@gmail.com>
2019-11-16 12:16:25 +0100

    .

--- Repostar/snakes/snakesule.py
+++ Repostar/snakes/snakesule.py
@@ -6,20 +6,22 @@ import EasyG
 import os
 
 #  POPIS VREMENA U KOJIMA BOT OBJAVLJUJE
-vremena = ['00:00', '05:00', '16:00']
+vremena = ['05:00', '16:00']
 vremena.sort()
 
 user = os.path.basename(__file__)[:-3]
 print('Kao [' + user + '] objavljujem u ' + str(vremena))
 
+
 def job():
-	while True:
-		if EasyG.jobbanje(user):
-			print('Dunno what does but works')
-			sleep(2)
-		else:
-			print('Jedan manje, keep it up')
-			break
+    while True:
+        if EasyG.jobbanje(user):
+            print('Dunno what does but works')
+            sleep(2)
+        else:
+            print('Jedan manje, keep it up')
+            break
+
 
 #  TERMINI OBAJVA IDU OD NAJRANIJEG DO NAJKASNIJEG VREMENA
 for vrijeme in vremena:
